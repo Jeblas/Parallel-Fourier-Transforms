42ee4559143e3c3aeb83544c0dc4b480d6f104dc
jblasco6 <john.e.blasco@gmail.com>
2018-12-02 15:22:32 -0500

    initial project structure

---
+++ README.txt
@@ -0,0 +1,1 @@
+Your CMakeLists.txt file should be in the top level folder along with your final report.  Other than that, I don't care how you organize your files within your solution.
---
+++ complex.cc
@@ -0,0 +1,53 @@
+//
+// Created by brian on 11/20/18.
+//
+
+#include "complex.h"
+
+#include <cmath>
+
+const float PI = 3.14159265358979f;
+
+Complex::Complex() : real(0.0f), imag(0.0f) {}
+
+Complex::Complex(float r) : real(r), imag(0.0f) {}
+
+Complex::Complex(float r, float i) : real(r), imag(i) {}
+
+Complex Complex::operator+(const Complex &b) const {
+
+}
+
+Complex Complex::operator-(const Complex &b) const {
+
+}
+
+Complex Complex::operator*(const Complex &b) const {
+
+}
+
+Complex Complex::mag() const {
+
+}
+
+Complex Complex::angle() const {
+
+}
+
+Complex Complex::conj() const {
+
+}
+
+std::ostream& operator<< (std::ostream& os, const Complex& rhs) {
+    Complex c(rhs);
+    if(fabsf(rhs.imag) < 1e-10) c.imag = 0.0f;
+    if(fabsf(rhs.real) < 1e-10) c.real = 0.0f;
+
+    if(c.imag == 0) {
+        os << c.real;
+    }
+    else {
+        os << "(" << c.real << "," << c.imag << ")";
+    }
+    return os;
+}
