76b01b774dac048b182617e26933c089ab62e66a
John Blasco <jeblas@Lynx.local>
2018-12-04 20:51:57 -0500

    started fft

--- src/single-thread-cooley-turkey.cc
+++ src/single-thread-cooley-turkey.cc
@@ -3,6 +3,23 @@
 
 #include <iostream>
 
+const float PI = 3.14159265358979f;
+
+void fft(Complex *in, int size, int step) {
+    if (size <= 1) {
+        return;
+    }
+
+    //divide
+
+    //recursive call
+
+    //combine
+
+
+
+}
+
 int main(int argc, char **argv) {
     Complex *in_img;
     Complex *out_img;
