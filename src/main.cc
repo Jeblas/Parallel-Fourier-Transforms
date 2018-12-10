5f255bf1b40df12ce66ef709b34d58337818e57e
John Blasco <jeblas@lawn-128-61-70-4.lawn.gatech.edu>
2018-12-10 15:43:15 -0500

    organized source code structure

--- src/main.cc
+++ src/main.cc
@@ -1,18 +1,26 @@
-#include "Multi_Threaded/threads_dtf.cc"
-#include "MPI/mpi_fft.cc"
-#include "Cuda/cuda_naive.cu"
+#include "./Multi_Threaded/threads_dtf.cc"
+#include "./MPI/mpi_fft.h"
+#include "Cuda/cuda_naive.h"
+
+#include <iostream>
 
 int main(int argc, char **argv) {
     // check for correct number of arguments
-
+    if (argc != 4) {
+        std::cout << "Incorrect number of arguments:";
+        std::cout << " (forward|reverse) <input_file.txt> <output_file.txt\n>";
+        return -1;
+    }
     //define tags for the variants
-    // main_multi(argc, argv);
-    // main_mpi(argc, argv);
-    // main_cuda(argc, argv);
-
-    
-    // check for reverse argument
-    
+    if (argv[1] == "forward") {
+        // main_multi_forward(argc, argv);
+        // main_mpi_forward(argc, argv);
+        // main_cuda_forward(argc, argv);
+    } else {
+        // main_multi_reverse(argc, argv);
+        // main_mpi_reverse(argc, argv);
+        // main_cuda_reverse(argc, argv);
+    }
 
     return 0;
 }
